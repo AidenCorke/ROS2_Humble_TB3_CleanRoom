be03f4e1748ab4523586038591ca954ac33355ff
Aiden Corke <aiden.corke@gmail.com>
2026-04-03 16:05:18 -0400

    Finished basic navigator functions for navigating to point and waypoints, testing to be completed.

--- clean_room_server/clean_room_server/clean_room_server.py
+++ clean_room_server/clean_room_server/clean_room_server.py
@@ -18,6 +18,9 @@ from nav2_msgs.action import FollowWaypoints, NavigateToPose
 from rclpy.executors import MultiThreadedExecutor
 from action_msgs.msg import GoalStatusArray
 from nav2_msgs.action._follow_waypoints import FollowWaypoints_FeedbackMessage
+from nav2_simple_commander.robot_navigator import BasicNavigator, TaskResult
+from rclpy.duration import Duration
+
 
 # Standard Imports
 import asyncio
@@ -59,25 +62,6 @@ class CleanRoomServer(Node):
                                            execute_callback=self.execute_callback,  # Action execution
                                            cancel_callback=self.cancel_callback     # Action canceling
                                            )
-
-        # Create Nav2 action clients
-        self.follow_wp_client = ActionClient(self,
-                                             FollowWaypoints,
-                                             'follow_waypoints'
-                                             )
-        self.nav_client = ActionClient(self,
-                                       NavigateToPose,
-                                       'navigate_to_pose'
-                                       )
-
-        # Subscribe to feedback topic from nav2
-        self.current_wp_index = 0
-        self.wp_feedback_sub = self.create_subscription(
-            FollowWaypoints_FeedbackMessage,
-            '/follow_waypoints/_action/feedback',
-            self.wp_feedback_callback,
-            10
-        )
 
         # Subscribe to robot's current pose
         self.current_pose = None
@@ -131,49 +115,106 @@ class CleanRoomServer(Node):
     # -------------------------------------------------------------------
     # --- Navigate to entry point of room ---
     # -------------------------------------------------------------------
-    async def navigate_to_entry_point(self, room_name):
-        """This function sends a nav2 request to navigate to the entry point of the given room."""
+    def navigate_to_entry_point(self, room_name):
+        """This function sends a nav2 request to navigate to the entry point of the given room using the simple commander basic navigator class."""
         # Extract entry point from param files
         params = self.manager.room_config[room_name]
         entry_px, entry_py = params["entry_point"]
         xw, yw = self.manager.map_utils.pixel_to_world(entry_px, entry_py)
 
-        # Initialize goal for Nav2
-        goal = NavigateToPose.Goal()
-        pose = PoseStamped()
-        pose.header.frame_id = "map"
-        pose.header.stamp = self.get_clock().now().to_msg()
-        pose.pose.position.x = float(xw)
-        pose.pose.position.y = float(yw)
-        pose.pose.orientation.w = 1.0       # Face forward
-        goal.pose = pose
+        # Create Nav2 BasicNavigator instance
+        navigator = BasicNavigator()
+
+        # Set the initial (current) pose
+        navigator.setInitialPose(self.current_pose)
+
+        # Wait for navigation to fully activate, since autostarting nav2
+        navigator.waitUntilNav2Active()
+
+        # Load map file
+        #navigator.changeMap(self.get_parameter('map_yaml').value)
+
+        # Obtain global and local costmaps
+        #self.global_costmap = self.navigator.getGlobalCostmap()
+        #self.local_costmap = self.navigator.getLocalCostmap()
+
+        # Create waypoint goal for navigator
+        goal = PoseStamped()
+        goal.header.frame_id = "map"
+        goal.header.stamp = self.get_clock().now().to_msg()
+        goal.pose.position.x = float(xw)
+        goal.pose.position.y = float(yw)
+        goal.pose.orientation.w = 1.0       # Face forward
+        goal.pose.orientation.z = 0.0       
 
         self.get_logger().info(f"Navigating to entry point of {room_name}...")
 
-        # Wait to send goal
-        self.nav_client.wait_for_server()  # Wait for server to be free
-        future = self.nav_client.send_goal_async(goal)  # Send async goal request
-        goal_handle = await future
+        nav_start = self.navigator.get_clock().now()    # Time of nav start
 
-        # Rejection handling
-        if not goal_handle.accepted:
-            self.get_logger().error("Entry point navigation rejected")
-            return False
+        nav_to_point = navigator.goToPose(goal)
 
-        # Wait for result
-        result_future = goal_handle.get_result_async()
-        await result_future
+        while not navigator.isTaskComplete(task=nav_to_point):
+            
+            i += 1
+            feedback = navigator.getFeedback(task=nav_to_point)
+            if feedback and i % 5 == 0:
+                print( 'Estimated time of arrival: '
+                    + '{:.0f}'.format(
+                        Duration.from_msg(feedback.estimated_time_remaining).nanoseconds
+                        / 1e9
+                    )
+                    + ' seconds.'
+                )
 
-        self.get_logger().info("Reached entry point")
+                print(
+                    'Distance remaining: '
+                    + '{:.2f}'.format(feedback.distance_remaining)
+                    + ' meters.'
+                )            
+                
+                # Cancel navigation is it takes 10 minutes
+                if Duration.from_msg(feedback.navigation_time) > Duration(seconds=600.0):
+                    navigator.cancelTask()
+            
+
+        # -- Results --    
+        result = navigator.getResult()  # Extract result
+        # Task succeeded
+        if result == TaskResult.SUCCEEDED:                                  
+            self.get_logger().info('Reached entry point.')
+
+        # Task canceled
+        elif result == TaskResult.CANCELED:                                 
+            self.get_logger().info('Navigation to entry point canceled!')
+
+        # Task failed
+        elif result == TaskResult.FAILED:                                   
+            (error_code, error_msg) = navigator.getTaskError()
+            self.get_logger().warn(f'Entry point navigation failed!{error_code}:{error_msg}')
+
+        # Weird result
+        else:                                                              
+            self.get_logger().warn('Entry point navigation has an invalid return status!')        
+        
+        navigator.lifecycleShutdown()   # Shutdown navigator instance
+
+        
         return True
 
 
     # -------------------------------------------------------------------
     # --- Navigate through cleaning path ---
     # -------------------------------------------------------------------
-    async def navigate_waypoints(self, ordered_waypoints):
+    def navigate_waypoints(self, ordered_waypoints):
         """This function sends a list of (x, y) waypoints to Nav2 FollowWaypoints."""
-        poses = []
+
+        # -- Initialize navigator instance --
+        navigator = BasicNavigator()                    # Initialize navigator class instance
+        navigator.setInitialPose(self.current_pose)     # Set the initial (current) pose
+        navigator.waitUntilNav2Active()                 # Wait for nav2 to fully activate, since autostarting nav2
+
+        # -- Convert poses into readable goals for nav2 --        
+        goal_poses = []
         total = len(ordered_waypoints)
 
         # Loop through all waypoints adding to pose
@@ -192,52 +233,51 @@ class CleanRoomServer(Node):
                 yaw = 0.0  # final waypoint
 
             pose.pose.orientation = yaw_to_quaternion(yaw)
-            poses.append(pose)
+            goal_poses.append(pose)
 
-        # Initialize goal
-        goal = FollowWaypoints.Goal()
-        goal.poses = poses
+        nav_start = navigator.get_clock().now()
+        nav_to_wp = navigator.followWaypoints(goal_poses)
 
-        # Wait for a server to be available
-        self.get_logger().info(f"Sending {len(poses)} waypoints to FollowWaypoints")
-        self.follow_wp_client.wait_for_server(10)
-        self.get_logger().info("FollowWaypoints server is available")
+        while not navigator.isTaskComplete(task=nav_to_wp):
+            
+            i = i + 1
+            feedback = navigator.getFeedback(task=nav_to_wp)
+            if feedback and i % 5 == 0:
+                print(
+                    'Executing current waypoint: '
+                    + str(feedback.current_waypoint + 1)
+                    + '/'
+                    + str(len(goal_poses))
+                )
+                now = navigator.get_clock().now()           
+                    
+                # Cancel navigation is it takes 10 minutes
+                if Duration.from_msg(feedback.navigation_time) > Duration(seconds=600.0):
+                        navigator.cancelTask()
+            
+        # -- Results --    
+        result = navigator.getResult()  # Extract result
 
-        future = self.follow_wp_client.send_goal_async(goal)
-        goal_handle = await future  # Wait for goal
+        # Task succeeded
+        if result == TaskResult.SUCCEEDED:                                  
+            self.get_logger().info('Cleaning completed successfully!')
+
+        # Task canceled
+        elif result == TaskResult.CANCELED:                                 
+            self.get_logger().info('Cleaning was cancelled!')
         
-        # Rejection handling
-        if not goal_handle.accepted:
-            self.get_logger().error("Waypoint navigation goal rejected")
-            return False
+        # Task failed
+        elif result == TaskResult.FAILED:                                   
+            (error_code, error_msg) = navigator.getTaskError()
+            self.get_logger().warn(f'Cleaning failed!{error_code}:{error_msg}')
 
-        # Wait for result
-        self.get_logger().info(f"Goal accepted: {goal_handle.accepted}")
-        result_future = goal_handle.get_result_async()
+        # Weird result
+        else:
+            self.get_logger().warn('Goal has an invalid return status!')        
+        
+        navigator.lifecycleShutdown()   # Shutdown navigator instance once done
 
-        # Update feedback message
-        feedback_msg = CleanRoom.Feedback() # Create variable for storing feedback in form of action
-        feedback_msg.status = "Cleaning in progress..."
-
-
-        # ------------- make into timer fct?
-        while not result_future.done(): # Loops while the result isnt finished
-            progress = (self.current_wp_index / total) * 100
-            feedback_msg.progress = float(progress)
-            goal_handle.publish_feedback(feedback_msg) # <-------------- error here
-            await asyncio.sleep(0.5)
-
-        result = await result_future
-        self.get_logger().info(f"FollowWaypoints result: {result.result}")
-        self.get_logger().info("Waypoint navigation completed")
         return True
-
-
-    # -------------------------------------------------------------------
-    # --- Execute action ---
-    # -------------------------------------------------------------------
-    def wp_feedback_callback(self, msg):
-        self.current_wp_index = msg.feedback.current_waypoint
 
 
     # -------------------------------------------------------------------
