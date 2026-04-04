1d1ef40119ad63bc413ebdca49a292084727d28d
Aiden Corke <aiden.corke@gmail.com>
2026-04-04 16:26:14 -0400

    Removed extra packages

--- clean_room_operations/build/clean_room_operations/share/clean_room_operations/hook/pythonpath_develop.sh
+++
@@ -1,3 +0,0 @@
-# generated from colcon_core/shell/template/hook_prepend_value.sh.em
-
-_colcon_prepend_unique_value PYTHONPATH "/home/aiden/ros2_ws_lab/src/clean_room_operations/build/clean_room_operations"
